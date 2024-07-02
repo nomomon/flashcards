99e4724ecf869f614b800d3d75c2570da02adc61
nomomon <funisinfinity26@gmail.com>
2024-07-02 17:59:55 +0500

    add data persistence; unlimited storage

--- lib/backend/index.ts
+++ lib/backend/index.ts
@@ -1,22 +1,26 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { CACHE_SIZE_UNLIMITED, persistentLocalCache } from "firebase/firestore";
+import { collection, getDocs, initializeFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
-  apiKey: "REDACTED_FIREBASE_WEB_API_KEY",
-  authDomain: "learning-arabic-95ec9.firebaseapp.com",
-  projectId: "learning-arabic-95ec9",
-  storageBucket: "learning-arabic-95ec9.appspot.com",
-  messagingSenderId: "593320075808",
-  appId: "1:593320075808:web:a01917e758cf40ff391245",
-  measurementId: "G-N6864P4G7H",
+    apiKey: "REDACTED_FIREBASE_WEB_API_KEY",
+    authDomain: "learning-arabic-95ec9.firebaseapp.com",
+    projectId: "learning-arabic-95ec9",
+    storageBucket: "learning-arabic-95ec9.appspot.com",
+    messagingSenderId: "593320075808",
+    appId: "1:593320075808:web:a01917e758cf40ff391245",
+    measurementId: "G-N6864P4G7H",
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = initializeFirestore(app, { //@ts-ignore
+    localCache: persistentLocalCache({}),
+    cacheSizeBytes: CACHE_SIZE_UNLIMITED
+});
 
 export async function getDecks() {
-  const decksCol = collection(db, "decks");
-  const deckSnapshot = await getDocs(decksCol);
-  const deckList = deckSnapshot.docs.map((doc) => doc.data());
-  return deckList;
+    const decksCol = collection(db, "decks");
+    const deckSnapshot = await getDocs(decksCol);
+    const deckList = deckSnapshot.docs.map((doc) => doc.data());
+    return deckList;
 }
