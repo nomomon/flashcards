5fb48861933a437b936127a3189df4bcf6a2c229
nomomon <funisinfinity26@gmail.com>
2024-07-04 15:23:51 +0500

    messed up solution

--- components/templates/learn/flipable-card.tsx
+++ components/templates/learn/flipable-card.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 
 interface FlipableCardProps {
   front: string;
@@ -6,19 +6,29 @@ interface FlipableCardProps {
 }
 
 const FlipableCard: FC<FlipableCardProps> = ({ front, back }) => {
-  const [flipped, setFlipped] = useState(false);
+  const flipCard = () => {
+    const card = document.querySelectorAll(".flipable-card");
+    card.forEach((el) => {
+      const flipped = el.getAttribute("data-flipped");
+      el.setAttribute("data-flipped", flipped === "true" ? "false" : "true");
+    });
+  };
   return (
     <div
       className="max-w-sm bg-white rounded-xl shadow-md mx-auto my-10 p-5 aspect-square flex justify-center items-center text-6xl cursor-pointer"
-      onClick={() => setFlipped(!flipped)}
+      onClick={flipCard}
     >
       <div
-        className={`w-full h-full flex justify-center items-center ${flipped ? "hidden" : ""}`}
+        key={front}
+        data-flipped="false"
+        className={`w-full h-full  justify-center items-center flipable-card flex data-[flipped=true]:hidden`}
       >
         {front}
       </div>
       <div
-        className={`w-full h-full flex justify-center items-center ${flipped ? "" : "hidden"}`}
+        key={back}
+        data-flipped="false"
+        className={`w-full h-full justify-center items-center flipable-card hidden data-[flipped=true]:flex`}
       >
         {back}
       </div>
--- components/templates/learn/index.tsx
+++ components/templates/learn/index.tsx
@@ -44,11 +44,7 @@ const LearnDeck: FC<LearnDeckProps> = ({ deck, deckProgress }) => {
 
   return (
     <div className="w-full h-full">
-      <FlipableCard
-        front={wordList[0].front}
-        back={wordList[0].back}
-        key={wordList[0].front}
-      />
+      <FlipableCard front={wordList[0].front} back={wordList[0].back} />
       <div className="max-w-md mx-auto flex justify-between">
         <FeedbackButton onClick={() => updateProgress(0)} text="Incorrect">
           <CornerUpLeftIcon />
