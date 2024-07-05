acb8063450795f9a2f333aaecfeb5dd30d87e907
nomomon <funisinfinity26@gmail.com>
2024-07-05 17:39:52 +0500

    simplify the styles

--- components/templates/overview/loading.tsx
+++ components/templates/overview/loading.tsx
@@ -8,10 +8,7 @@ const OverviewLoading = () => {
       </div>
       <div className="w-full grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {[...Array(8)].map((_, i) => (
-          <Skeleton
-            key={i}
-            className="flex flex-col justify-between p-4 text-white rounded-md aspect-square"
-          />
+          <Skeleton key={i} className="rounded-md aspect-square" />
         ))}
       </div>
     </>
