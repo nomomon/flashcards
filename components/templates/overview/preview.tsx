1b5e293466a27dd7e037378598471d0e90b6e3bb
nomomon <funisinfinity26@gmail.com>
2024-07-05 18:37:27 +0500

    add tags to words

--- components/templates/overview/preview.tsx
+++ components/templates/overview/preview.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
   Drawer,
@@ -8,6 +9,7 @@ import {
   DrawerHeader,
   DrawerTitle,
 } from "@/components/ui/drawer";
+import _ from "lodash";
 import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
 import React, { FC } from "react";
@@ -20,19 +22,30 @@ interface DeckPreviewProps {
 const DeckPreview: FC<DeckPreviewProps> = ({ deck, setSelectedDeck }) => {
   const close = () => setSelectedDeck(undefined);
 
+  const uniqueTags = _.uniq(deck.words.flatMap((wordPair) => wordPair.tags));
+
   return (
     <Drawer open={!!deck} onClose={close}>
-      <DrawerContent className="bg-muted h-[calc(100vh-2rem)] max-w-lg mx-auto">
+      <DrawerContent className="bg-muted h-[calc(100vh-2rem)] max-w-lg mx-auto focus:outline-none">
         <DrawerHeader>
           <DrawerTitle>{deck.name}</DrawerTitle>
-          <DrawerDescription>
-            <div>{deck.words.length} word pairs</div>
-            <div className="flex gap-1 items-center">
+          <div className="w-full flex flex-col gap-2 text-sm overflow-hidden">
+            <div className="text-muted-foreground">
+              {deck.words.length} word pairs
+            </div>
+            <div className="flex gap-1 items-center text-muted-foreground max-sm:justify-center">
               {deck.languages.front.slice(0, 2).toUpperCase()}
               <ArrowRightIcon className="w-4 h-4" />
               {deck.languages.back.slice(0, 2).toUpperCase()}
             </div>
-          </DrawerDescription>
+            <div className="max-w-full overflow-hidden">
+              <div className="flex gap-1 overflow-x-auto scrollbar-hide">
+                {uniqueTags.map((tag, idx) => (
+                  <Badge key={idx}>#{tag}</Badge>
+                ))}
+              </div>
+            </div>
+          </div>
         </DrawerHeader>
         <div className="p-4 flex flex-col gap-2 max-h-[90vh] overflow-scroll">
           {deck.words.map((wordPair, idx) => (
