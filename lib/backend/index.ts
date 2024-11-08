08b3716ca1f7893ab0e58d1e049432cff06677a6
nomomon <funisinfinity26@gmail.com>
2024-11-08 14:50:11 +0100

    rename folders; update firebase id

--- lib/backend/index.ts
+++ lib/backend/index.ts
@@ -1,60 +1,60 @@
 import { initializeApp } from "firebase/app";
 import {
-  CACHE_SIZE_UNLIMITED,
-  getDocFromCache,
-  persistentLocalCache,
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  initializeFirestore,
-  getDocsFromCache,
-  setDoc,
+    CACHE_SIZE_UNLIMITED,
+    getDocFromCache,
+    persistentLocalCache,
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    initializeFirestore,
+    getDocsFromCache,
+    setDoc,
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "REDACTED_FIREBASE_WEB_API_KEY",
-  authDomain: "learning-arabic-95ec9.firebaseapp.com",
-  projectId: "learning-arabic-95ec9",
-  storageBucket: "learning-arabic-95ec9.appspot.com",
-  messagingSenderId: "593320075808",
-  appId: "1:593320075808:web:a01917e758cf40ff391245",
-  measurementId: "G-N6864P4G7H",
+    apiKey: "REDACTED_FIREBASE_WEB_API_KEY",
+    authDomain: "learning-flashcards.firebaseapp.com",
+    projectId: "learning-flashcards",
+    storageBucket: "learning-flashcards.firebasestorage.app",
+    messagingSenderId: "848972481460",
+    appId: "1:848972481460:web:6bd7bec44bb71d713a28e1",
+    measurementId: "G-2S3K85FW9V"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = initializeFirestore(app, {
-  //@ts-ignore
-  localCache: persistentLocalCache({}),
+    //@ts-ignore
+    localCache: persistentLocalCache({}),
 });
 
 export async function getDecks(useCache: boolean = true) {
-  const decksCol = collection(db, "decks");
-  const deckSnapshot = await (useCache ? getDocsFromCache : getDocs)(decksCol);
-  const deckList = deckSnapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
-  return deckList;
+    const decksCol = collection(db, "decks");
+    const deckSnapshot = await (useCache ? getDocsFromCache : getDocs)(decksCol);
+    const deckList = deckSnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+    }));
+    return deckList;
 }
 
 export async function getDeck(deckId: string) {
-  const decksCol = collection(db, "decks");
-  const deckDoc = await getDocFromCache(doc(decksCol, deckId)).catch(() =>
-    getDoc(doc(decksCol, deckId)),
-  );
-  if (deckDoc.exists()) {
-    return {
-      id: deckDoc.id,
-      ...deckDoc.data(),
-    } as Deck;
-  } else {
-    throw new Error("Deck not found");
-  }
+    const decksCol = collection(db, "decks");
+    const deckDoc = await getDocFromCache(doc(decksCol, deckId)).catch(() =>
+        getDoc(doc(decksCol, deckId)),
+    );
+    if (deckDoc.exists()) {
+        return {
+            id: deckDoc.id,
+            ...deckDoc.data(),
+        } as Deck;
+    } else {
+        throw new Error("Deck not found");
+    }
 }
 
 export async function updateDeck(deck: Deck) {
-  const decksCol = collection(db, "decks");
-  const newDeck = await setDoc(doc(decksCol, deck.id), deck);
-  return newDeck;
+    const decksCol = collection(db, "decks");
+    const newDeck = await setDoc(doc(decksCol, deck.id), deck);
+    return newDeck;
 }
