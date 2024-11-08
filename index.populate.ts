4c143278ec0cffd2fe39d50271c844c56f21fbe2
nomomon <funisinfinity26@gmail.com>
2024-11-08 15:42:25 +0100

    refactor: remove unused Dutch deck JSON and improve code formatting across multiple files

--- index.populate.ts
+++ index.populate.ts
@@ -5,19 +5,22 @@ import { updateDeck } from "./lib/backend";
 const decksDir = join(__dirname, "public/decks");
 
 try {
-  const files = readdirSync(decksDir);
-  files.forEach((file) => {
-    if (file.endsWith(".json")) {
-      try {
-        const filePath = join(decksDir, file);
-        const fileContent = readFileSync(filePath, "utf8");
-        const deck = JSON.parse(fileContent);
-        updateDeck(deck);
-      } catch (error) {
-        console.error(`Error processing file ${file}:`, error);
-      }
-    }
-  });
+    const files = readdirSync(decksDir);
+    files.forEach((file) => {
+        if (file.endsWith(".json")) {
+            try {
+                const filePath = join(decksDir, file);
+                const fileContent = readFileSync(filePath, "utf8");
+                const deck = JSON.parse(fileContent);
+                updateDeck(deck);
+            } catch (error) {
+                console.error(`Error processing file ${file}:`, error);
+            }
+        }
+    });
 } catch (error) {
-  console.error("Error reading decks directory:", error);
+    console.error("Error reading decks directory:", error);
 }
+
+console.log("Decks populated");
+process.exit();
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,14 +2,18 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export function shuffle<T>(array: T[]): T[] {
-  const newArray = [...array];
-  for (let i = newArray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-  }
-  return newArray;
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
 }
+
+export function isBrowser() {
+    return typeof window !== "undefined" && typeof window.document !== "undefined";
+};
